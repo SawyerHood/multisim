ee1f7b74ab5a7f4a36a6e75c7c3b29210954d848
Sawyer Hood <sawyerjhood@gmail.com>
2024-05-18 12:14:39 -0700

    Make link clicking work

--- public/bootstrap.js
+++ public/bootstrap.js
@@ -1,3 +1,10 @@
 window.addEventListener("mousemove", (e) => {
   window.parent.postMessage({ type: "mousemove", x: e.pageX, y: e.pageY }, "*");
 });
+
+window.addEventListener("click", (e) => {
+  if (e.target.tagName === "A") {
+    e.preventDefault();
+    window.parent.postMessage({ type: "linkClick", href: e.target.href }, "*");
+  }
+});
